e22d952e363bab614e1d3e717915bac46e472137
Renat <gadievforever@gmail.com>
2024-06-03 17:15:00 +0300

    [modified] Bug fixes

--- script5_2.js
+++ script5_2.js
@@ -1,39 +1,22 @@
-const menuData = [
-    { title: "1", href: "#page-1" },
-    { title: "2", href: "#page-2" },
-    { title: "3", href: "#page-3" },
-  ];
-  
-  const mainMenu = document.getElementById("main-menu");
-  const pageContent = document.getElementById("main-content");
-  
-  function generateMenu(menuItems, parentElement) {
-    const ul = document.createElement("ul");
-    parentElement.appendChild(ul);
-  
-    menuItems.forEach(menuItem => {
-      const li = document.createElement("li");
-      const link = document.createElement("a");
-      link.textContent = menuItem.title;
-      link.href = menuItem.href;
-  
-      link.addEventListener("click", function(event) {
-        event.preventDefault(); // Запобігаємо перезавантаженню сторінки
-        showPage(menuItem.href); // Відображаємо відповідну сторінку
-      });
-  
-      li.appendChild(link);
-      ul.appendChild(li);
-    });
-  }
-  
-  function showPage(pageId) {
-    const pages = document.querySelectorAll("[id^='page-']"); // Вибираємо всі елементи з ID, що починаються з "page-"
-    pages.forEach(page => page.style.display = "none"); // Приховуємо всі сторінки
-    const pageToShow = document.querySelector(pageId); // Знаходимо сторінку за ID
-    if (pageToShow) pageToShow.style.display = "block"; // Відображаємо знайдену сторінку
-  }
-  
-  generateMenu(menuData, mainMenu);
-  showPage("#page-1"); // Відображаємо сторінку 1 за замовчуванням
-  
+const mainMenu = document.querySelector('.main-menu');
+const contentContainer = document.querySelector('#content-container');
+
+mainMenu.addEventListener('click', function(event) {
+  event.preventDefault(); 
+  const target = event.target;
+  const link = target.closest('a');
+    loadLayout(link.id);
+
+});
+
+function loadLayout(layoutId) {
+  const layoutUrl = `layouts/${layoutId}.html`; 
+  const xhr = new XMLHttpRequest(); 
+  xhr.open('GET', layoutUrl);
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      contentContainer.innerHTML = xhr.responseText; 
+    }
+  };
+  xhr.send(); 
+}
